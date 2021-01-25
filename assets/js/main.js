788e234f60579a4dd42afc6fc82cff2dcc990f59
aaditya joshi <cloudaaditya@gmail.com>
2021-01-25 18:00:14 +0545

    multy_store code

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+$('.multy-store-bg-item').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    prevArrow: "",
+    nextArrow: "",
+    dots: true,
+    infinite: true,
+    speed: 2000,
+    fade: true,
+    cssEase: 'ease',
+});
